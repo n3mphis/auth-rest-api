6a080f069b6cbc98c012d5c04bcc8cb70caad4f4
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-16 19:10:01 -0300

    Creado el metodo en SecurityConfig para encriptar contraseñas y el filter chain

---
+++ src/main/java/com/n3mphis/auth_rest_api/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.n3mphis.auth_rest_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/login", "/register").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
