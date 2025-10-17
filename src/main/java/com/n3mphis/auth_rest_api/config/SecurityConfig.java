68f7f0120561df3473ddd0b0d9afa400b06b840d
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-17 16:29:11 -0300

    Creado el RoleRepository y cambios en SecurityConfig

--- src/main/java/com/n3mphis/auth_rest_api/config/SecurityConfig.java
+++ src/main/java/com/n3mphis/auth_rest_api/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.n3mphis.auth_rest_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,9 +18,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/login", "/register").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/superAdmin").hasRole("SUPER_ADMIN")
                         .anyRequest().authenticated());
 
         return http.build();
---
+++ src/main/java/com/n3mphis/auth_rest_api/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.n3mphis.auth_rest_api.repository;
+
+import com.n3mphis.auth_rest_api.model.Role;
+import com.n3mphis.auth_rest_api.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleName name);
+}
